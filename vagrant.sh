5240a71d9d9c2b31a24ef79db2e2f94e80c0fe6f
Jonas Eckerström <jonaseck@gmail.com>
2016-04-15 21:25:47 +0200

    Fixed new vagrant download url

--- vagrant.sh
+++ vagrant.sh
@@ -5,21 +5,8 @@
 
 sudo apt-get -qqy purge virtualbox
 
-vagrant_version=`curl -w %{redirect_url} https://bintray.com/mitchellh/vagrant/vagrant/_latestVersion`
-vagrant_version=`echo ${vagrant_version##*/vagrant/}`
-vagrant_version=`echo ${vagrant_version%/*}`
-vagrant_url=https://dl.bintray.com/mitchellh/vagrant/vagrant_${vagrant_version}_x86_64.deb
-
+vagrant_version=`curl -sL https://releases.hashicorp.com/vagrant/ | sed -n -e 's#.*/vagrant/\([^/]*\).*#\1#p' | head -1`
+vagrant_url=https://releases.hashicorp.com/vagrant/${vagrant_version}/vagrant_${vagrant_version}_x86_64.deb
 curl -JLOs $vagrant_url
-
 sudo dpkg -i ${vagrant_url##*/}
 rm -f ${vagrant_url##*/}
-
-mkdir -p ~/.gnupg
-chmod 700 ~/.gnupg
-touch ~/.gnupg/gpg.conf
-chmod 600 ~/.gnupg/gpg.conf
-sudo sh -c "echo deb http://download.virtualbox.org/virtualbox/debian trusty contrib > /etc/apt/sources.list.d/virtualbox.list"
-curl -s http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | sudo apt-key add -
-sudo apt-get -qq update
-sudo apt-get -qqy install -qqy virtualbox
